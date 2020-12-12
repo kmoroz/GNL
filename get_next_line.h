974d2b6eba2c5c3a6b06dabe82e59ea1f94d25ca
Ksenia Morozova <ksmorozo@f0r3s26.codam.nl>
2020-12-12 12:32:01 +0100

    add buff_size condition

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/10 13:42:20 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2020/12/10 13:49:28 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2020/12/12 10:17:52 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define GET_NEXT_LINE_H
 
 # include <stddef.h>
-# define BUFF_SIZE 35
+# ifndef BUFF_SIZE
+#  define BUFF_SIZE 35
+# endif
 
 char	*ft_strjoin(char const *s1, char const *s2);
 int		get_next_line(int fd, char **line);
