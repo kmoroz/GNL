178756d2bdb9fa9b7857bf5b9d2de6e13d484d72
kmoroz <ksmorozova@hotmail.com>
2021-01-13 18:42:30 +0000

    add more malloc protections

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/09 09:44:38 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/01/08 17:06:35 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/01/13 18:39:44 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ t_read_data	get_read_data(int fd)
 	return (read_data);
 }
 
-int			get_line(char **new_line_ptr, t_read_data read_data,
+void		get_new_line(char **new_line_ptr, t_read_data read_data,
 char *remainder)
 {
 	*new_line_ptr = ft_strchr(read_data.buff, '\n');
@@ -79,7 +79,6 @@ char *remainder)
 		(*new_line_ptr)++;
 		ft_strlcpy(remainder, *new_line_ptr, ft_strlen(*new_line_ptr) + 1);
 	}
-	return (1);
 }
 
 int			get_next_line(int fd, char **line)
@@ -88,22 +87,23 @@ int			get_next_line(int fd, char **line)
 	char			*new_line_ptr;
 	static char		remainder[BUFFER_SIZE];
 	char			*temp;
-	int				line_assigned;
 
 	if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
 	read_data.amount_read = 1;
 	*line = check_remainder(remainder, &new_line_ptr);
+	if (!line)
+		return (-1);
 	while (!new_line_ptr && read_data.amount_read)
 	{
 		read_data = get_read_data(fd);
-		if (!read_data.read_successful || !line)
+		if (!read_data.read_successful)
 			return (-1);
-		line_assigned = get_line(&new_line_ptr, read_data, remainder);
-		if (!line_assigned)
-			return (-1);
+		get_new_line(&new_line_ptr, read_data, remainder);
 		temp = *line;
 		*line = ft_strjoin(*line, read_data.buff);
+		if (!line)
+			return (-1);
 		free(temp);
 		free(read_data.buff);
 	}
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/12 13:18:50 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/01/13 11:29:01 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/01/13 18:39:26 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ t_read_data	get_read_data(int fd)
 	return (read_data);
 }
 
-int			get_line(char **new_line_ptr, t_read_data read_data,
+void		get_new_line(char **new_line_ptr, t_read_data read_data,
 char *remainder)
 {
 	*new_line_ptr = ft_strchr(read_data.buff, '\n');
@@ -79,7 +79,6 @@ char *remainder)
 		(*new_line_ptr)++;
 		ft_strlcpy(remainder, *new_line_ptr, ft_strlen(*new_line_ptr) + 1);
 	}
-	return (1);
 }
 
 int			get_next_line(int fd, char **line)
@@ -88,22 +87,23 @@ int			get_next_line(int fd, char **line)
 	char			*new_line_ptr;
 	static char		remainder[1024][BUFFER_SIZE];
 	char			*temp;
-	int				line_assigned;
 
 	if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
 	read_data.amount_read = 1;
 	*line = check_remainder(remainder[fd], &new_line_ptr);
+	if (!line)
+		return (-1);
 	while (!new_line_ptr && read_data.amount_read)
 	{
 		read_data = get_read_data(fd);
-		if (!read_data.read_successful || !line)
+		if (!read_data.read_successful)
 			return (-1);
-		line_assigned = get_line(&new_line_ptr, read_data, remainder[fd]);
-		if (!line_assigned)
-			return (-1);
+		get_new_line(&new_line_ptr, read_data, remainder[fd]);
 		temp = *line;
 		*line = ft_strjoin(*line, read_data.buff);
+		if (!line)
+			return (-1);
 		free(temp);
 		free(read_data.buff);
 	}
